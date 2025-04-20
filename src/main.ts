d6bda6fa6a2c9fc3c31b00818c43ffeed9d59b88
liaojiayun <liaojiayun@bytedance.com>
2025-04-20 12:48:01 +0800

    feat: 引入 Tailwind 和 Element Plus

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from '@/App.vue'
 import router from './router'
+// 引入 Element Plus 的图标库
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 const app = createApp(App)
 
+// 注册 Element Plus 图标为全局组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
